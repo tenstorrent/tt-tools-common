b3efd4879419fc3c7831a565a9229494c3139e2c
ShLiTT <xiaoruli@tenstorrent.com>
2024-02-28 21:08:55 +0000

    Moved generation and parsing of reset config json file from tt-smi
    Signed-off-by: ShLiTT <xiaoruli@tenstorrent.com>

---
+++ tt_tools_common/reset_common/host_reset_log.py
@@ -0,0 +1,179 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+This file contains functions used to generate host logs that are compatible with elasticsearch.
+"""
+
+from __future__ import annotations
+import json
+import base64
+import inspect
+import datetime
+from pathlib import Path
+from typing import Any, Union, List, TypeVar, Generic, Tuple
+from pydantic import BaseModel
+from pydantic.fields import Field
+import os
+
+
+class Long(int):
+    ...
+
+
+class Keyword(str):
+    ...
+
+
+class Text(str):
+    ...
+
+
+class Date(datetime.datetime):
+    @classmethod
+    def build(cls, format: str):
+        cls.format = format
+
+    @classmethod
+    def get_mapping(cls):
+        return {"type": "date", "format": cls.format}
+
+
+def optional(*fields):
+    """Decorator function used to modify a pydantic model's fields to all be optional.
+    Alternatively, you can  also pass the field names that should be made optional as arguments
+    to the decorator.
+    Taken from https://github.com/samuelcolvin/pydantic/issues/1223#issuecomment-775363074
+    """
+
+    def dec(_cls):
+        for field in fields:
+            _cls.__fields__[field].required = False
+            _cls.__fields__[field].default = None
+        return _cls
+
+    if fields and inspect.isclass(fields[0]) and issubclass(fields[0], BaseModel):
+        cls = fields[0]
+        fields = cls.__fields__
+        return dec(cls)
+
+    return dec
+
+
+def type_to_mapping(type: Any):
+    """Converts a python type to an elasticsearch mapping type"""
+
+    if issubclass(type, float):
+        return {"type": "float"}
+    elif issubclass(type, bool):
+        return {"type": "boolean"}
+    elif issubclass(type, Long):
+        return {"type": "long"}
+    elif issubclass(type, int):
+        return {"type": "integer"}
+    elif issubclass(type, bytes):
+        return {"type": "binary"}
+    elif issubclass(type, Keyword):
+        return {"type": "keyword"}
+    elif issubclass(type, Text):
+        return {"type": "text"}
+    elif issubclass(type, str):
+        return {"type": "text", "fields": {"keyword": {"type": "keyword"}}}
+    elif issubclass(type, Date):
+        return type.get_mapping()
+    elif issubclass(type, datetime.date):
+        return {"type": "date", "format": "strict_date_optional_time||epoch_millis"}
+    elif issubclass(type, ElasticModel):
+        return {"type": "object", "properties": type.get_mapping()}
+    else:
+        raise NotImplementedError(f"Have not implemented mapping support for {type}")
+
+
+def field_to_mapping(info: Field):
+    """Converts a pydantic field to an elasticsearch mapping type"""
+    try:
+        # print(info.outer_type_, type(info.outer_type_))
+        if (
+            hasattr(info.outer_type_, "__origin__")
+            and info.outer_type_.__origin__ == Nested
+        ):
+            inner = type_to_mapping(info.type_)
+            if inner.get("type", None) == "object":
+                inner["type"] = "nested"
+            else:
+                inner = {"type": "nested", "properties": inner}
+            return inner
+        else:
+            return type_to_mapping(info.type_)
+    except NotImplementedError as exc:
+        raise NotImplementedError(
+            f"Have not implemented mapping support for {info}"
+        ) from exc
+
+
+def json_load_bytes(obj):
+    """Converts a json object to bytes"""
+    if "__type__" in obj:
+        if obj["__type__"] == "bytes":
+            return base64.b64decode(obj["bytes"].encode("ascii"))
+    return obj
+
+
+class ElasticModel(BaseModel):
+    """Base class for models that are compatible with elasticsearch"""
+
+    @classmethod
+    def get_mapping(cls):
+        mapping = {}
+        for name, info in cls.__fields__.items():
+            mapping[name] = field_to_mapping(info)
+
+        return mapping
+
+    # Will add the ability to save to elasticsearch as needed
+    # def save(self, index: str):
+    #     es.index(index=index, document=self.json())
+
+
+T = TypeVar("T", bound=ElasticModel)
+
+
+class Nested(list, Generic[T]):
+    ...
+
+
+@optional
+class PciResetDeviceInfo(ElasticModel):
+    """Model for storing information about a PCI reset device"""
+
+    pci_index: List[int]
+
+
+@optional
+class MoboReset(ElasticModel):
+    """Model for storing information about a mobo reset"""
+
+    nb_host_pci_idx: int
+    mobo: str
+    credo: List[str]
+    disabled_ports: List[str]
+
+
+@optional
+class HostResetLog(ElasticModel):
+    """Model for storing information about a host reset log"""
+
+    time: datetime.datetime
+    host_name: str
+    gs_tensix_reset: PciResetDeviceInfo
+    wh_link_reset: PciResetDeviceInfo
+    re_init_devices: bool
+    wh_mobo_reset: List[MoboReset]
+
+    def save_as_json(self, fname: Union[str, Path]):
+        expanded_fname = os.path.expanduser(fname)
+        os.makedirs(os.path.dirname(expanded_fname), exist_ok=True)
+        with open(expanded_fname, "w") as f:
+            raw_json = self.json(exclude_none=True)
+            reloaded_json = json.loads(raw_json)
+            json.dump(reloaded_json, f, indent=4)
---
+++ tt_tools_common/reset_common/reset_utils.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+This file contains functions used to generate and save reset logs
+"""
+
+import json
+import sys
+import datetime
+import os
+from pathlib import Path
+import host_reset_log as log
+from tt_tools_common.ui_common.themes import CMD_LINE_COLOR
+from tt_tools_common.utils_common.system_utils import get_host_info
+
+
+def generate_reset_logs(backend, result_filename: str = None):
+    """Generate and save reset logs"""
+    time_now = datetime.datetime.now()
+    date_string = time_now.strftime("%m-%d-%Y_%H:%M:%S")
+    log_filename = f"~/.config/tenstorrent/mobo_reset_config_{date_string}.json"
+    gs_pci_idx = []
+    wh_pci_idx = []
+    for i, dev in enumerate(backend.devices):
+        if dev.as_wh() and not dev.is_remote():
+            wh_pci_idx.append(i)
+        elif dev.as_gs():
+            gs_pci_idx.append(i)
+    reset_log = log.HostResetLog(
+        time=time_now,
+        host_name=get_host_info()["Hostname"],
+        gs_tensix_reset=log.PciResetDeviceInfo(pci_index=gs_pci_idx),
+        wh_link_reset=log.PciResetDeviceInfo(pci_index=wh_pci_idx),
+        re_init_devices=True,
+        wh_mobo_reset=[
+            log.MoboReset(
+                nb_host_pci_idx=0,
+                mobo="<MOBO NAME>",
+                credo=["<group id>:<credo id>", "<group id>:<credo id>"],
+                disabled_ports=["<group id>:<credo id>", "<group id>:<credo id>"],
+            ),
+            log.MoboReset(
+                nb_host_pci_idx=1,
+                mobo="<MOBO NAME>",
+                credo=["<group id>:<credo id>", "<group id>:<credo id>"],
+                disabled_ports=["<group id>:<credo id>", "<group id>:<credo id>"],
+            ),
+        ],
+    )
+    if result_filename:
+        dir_path = os.path.dirname(os.path.realpath(result_filename))
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
+        log_filename = result_filename
+    reset_log.save_as_json(log_filename)
+    return log_filename
+
+
+def parse_reset_input(value):
+    """Validate the reset inputs - either list of int pci IDs or a json config file"""
+    try:
+        # Attempt to parse as a JSON file
+        with open(value, "r") as json_file:
+            data = json.load(json_file)
+            return data
+    except (json.JSONDecodeError, FileNotFoundError):
+        # If parsing as JSON fails, treat it as a comma-separated list of integers
+        try:
+            return [int(item) for item in value.split(",")]
+        except ValueError:
+            print(
+                CMD_LINE_COLOR.RED,
+                "Invalid input! Provide list of comma separated numbers or a json file.\n To generate a reset json config file run tt-smi -g",
+                CMD_LINE_COLOR.ENDC,
+            )
+            sys.exit(1)
