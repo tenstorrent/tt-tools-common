e3298edd232cb779f4b898bf625123bb76930b93
Samridhi <sbansal@tenstorrent.com>
2024-03-06 11:53:29 -0500

    tests: Added unit tests for all device resets
    Signed-off-by: Samridhi <sbansal@tenstorrent.com>

---
+++ tt_tools_common/tests/test_mobo_reset.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Test mobo reset using reset config file
+"""
+from tt_tools_common.reset_common.galaxy_reset import GalaxyReset
+from tt_tools_common.reset_common.wh_reset import WHChipReset
+from tt_tools_common.reset_common.reset_utils import (
+    parse_reset_input,
+)
+
+# UPDATE THIS BEFORE RUNNING
+INPUT_FILE = "~/.config/tenstorrent/reset_config.json"
+WH_RESET_PCI_NUM = 4
+
+
+def main():
+    # SAMPLE MOBO RESET CONFIG
+
+    # mobo = "mobo-ce-44"
+    # credo = ["6:0", "6:1", "7:0", "7:1"]
+    # disabled_ports = [
+    #     "0:0",
+    #     "0:1",
+    #     "0:2",
+    #     "1:0",
+    #     "1:1",
+    #     "1:2",
+    #     "6:0",
+    #     "6:1",
+    #     "6:2",
+    #     "0:0",
+    #     "0:1",
+    #     "7:2",
+    # ]
+
+    # mobo_dict_list = [{"mobo": mobo, "credo": credo, "disabled_ports": disabled_ports}]
+    mobo_dict_list = parse_reset_input(INPUT_FILE)["wh_mobo_reset"]
+    mobo_reset_obj = GalaxyReset()
+    mobo_reset_obj.warm_reset_mobo(mobo_dict_list)
+    reset_obj = WHChipReset()
+    reset_obj.full_lds_reset(list(range(WH_RESET_PCI_NUM)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tt_tools_common/tests/test_wh_reset.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Reset test for gs tensix reset
+"""
+from pyluwen import detect_chips
+from tt_tools_common.reset_common.wh_reset import WHChipReset
+
+
+def main():
+    wh_pci_idx = []
+    devices = detect_chips()
+    for i, dev in enumerate(devices):
+        if dev.as_wh() and not dev.is_remote():
+            wh_pci_idx.append(i)
+
+    WHChipReset().full_lds_reset(pci_interfaces=wh_pci_idx)
+
+
+if __name__ == "__main__":
+    main()
