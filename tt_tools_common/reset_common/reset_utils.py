3ecbfdecb1d99be152705ec8cb0432fd88a9017f
Samridhi <sbansal@tenstorrent.com>
2024-03-14 15:29:22 -0400

    Update reset config generation to use pci_interface_id
    - Reset config now displays the dev id as seen in dev/tenstorrent
    - This fixes discrepency seen when resetting on IRD systems
    
    Signed-off-by: Samridhi <sbansal@tenstorrent.com>

--- tt_tools_common/reset_common/reset_utils.py
+++ tt_tools_common/reset_common/reset_utils.py
@@ -30,9 +30,9 @@ def generate_reset_logs(devices, result_filename: str = None):
     wh_pci_idx = []
     for i, dev in enumerate(devices):
         if dev.as_wh() and not dev.is_remote():
-            wh_pci_idx.append(i)
+            wh_pci_idx.append(dev.get_pci_interface_id())
         elif dev.as_gs():
-            gs_pci_idx.append(i)
+            gs_pci_idx.append(dev.get_pci_interface_id())
     reset_log = log.HostResetLog(
         time=time_now,
         host_name=get_host_info()["Hostname"],
