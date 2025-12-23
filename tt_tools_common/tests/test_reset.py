89128a39b87b34dad3641117afe10f457a1e8046
Petra Alexson <palexson@tenstorrent.com>
2025-12-22 19:39:11 -0500

    Convert existing tests to pytests
    Replace chip-specific reset tests with a generic chip reset test set. Remove gs
    reset test and mobo reset test. Reformat all tests to be modular pytests.
    
    Signed-off-by: Petra Alexson <palexson@tenstorrent.com>

---
+++ tt_tools_common/tests/test_reset.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Test suite for chip reset
+"""
+import pytest
+
+from tt_tools_common.reset_common.chip_reset import ChipReset
+
+def test_full_lds_reset(devices, requires_hardware):
+    """Test full LDS reset on all detected chips."""
+    pci_interfaces = list(range(len(devices)))
+    ChipReset().full_lds_reset(pci_interfaces=pci_interfaces)
